12069a55bbef27f95692186bcfc2313e5315ff81
Solonenko Stanislav <solonenkostas@gmail.com>
2022-08-25 15:00:13 +0200

    github connected

--- src/main/java/com/hillel/solonenko/lessons/homework3/MyApp2.java
+++ src/main/java/com/hillel/solonenko/lessons/homework3/MyApp2.java
@@ -3,7 +3,7 @@ package com.hillel.solonenko.lessons.homework3;
 public class MyApp2 {
 
     public static void main(String[] args) {
-        System.out.println("github_conected");
+        System.out.println("github conected");
 
 
     }
