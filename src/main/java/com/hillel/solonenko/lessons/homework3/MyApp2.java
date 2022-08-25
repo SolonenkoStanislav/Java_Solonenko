76e8319f3f7cfbc392df1365dd2ac03dd456c4ec
Solonenko Stanislav <solonenkostas@gmail.com>
2022-08-25 14:59:07 +0200

    added javas

--- src/main/java/com/hillel/solonenko/lessons/homework3/MyApp2.java
+++ src/main/java/com/hillel/solonenko/lessons/homework3/MyApp2.java
@@ -3,8 +3,8 @@ package com.hillel.solonenko.lessons.homework3;
 public class MyApp2 {
 
     public static void main(String[] args) {
-        System.out.println();
-        int privet1 = 300;
+        System.out.println("github_conected");
+
 
     }
 }
