aff46cb360c8b2c3998487bdee6b5392b64b6013
Solonenko Stanislav <solonenkostas@gmail.com>
2022-09-06 18:05:50 +0200

    6th FINALLY DONE

--- src/main/java/com/hillel/solonenko/lessons/homework6/Main6.java
+++ src/main/java/com/hillel/solonenko/lessons/homework6/Main6.java
@@ -5,15 +5,10 @@ public class Main6 {
     public static void main(String[] args) {
 
         int firstValue = 0;
-        for (int i = 0; i < 125; i++) {
-            if (
-                    ((i / 10 == 4) || (i / 10 == 9))
-                            ||
-                    ((i % 10 == 4) || (i % 10 == 9)))
-                if (
-                (i % 10 == 4) || (i % 10 == 9)) {
+        for (int i = 0; i < 155; i++) {
+            if
+                (((i / 10 == 4) || (i / 10 == 9))  ||  ((i % 10 == 4) || (i % 10 == 9)) || (i / 10 % 10 == 4)) {
                 continue;
-
             }
             firstValue++;
             System.out.println(i + " shuttle number - " + firstValue);
