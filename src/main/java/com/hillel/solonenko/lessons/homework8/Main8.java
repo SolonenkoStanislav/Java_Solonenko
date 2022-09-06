197a91b4f6890d09c27e8889aeea5919545fc18d
Solonenko Stanislav <solonenkostas@gmail.com>
2022-09-06 22:28:48 +0200

    8th done

--- src/main/java/com/hillel/solonenko/lessons/homework8/Main8.java
+++ src/main/java/com/hillel/solonenko/lessons/homework8/Main8.java
@@ -11,22 +11,24 @@ public class Main8 {
         int middleAge = 0;
         for (int i = 0; i < firstTeamPlayers.length; i++) {
             firstTeamPlayers[i] = (int) (Math.random() * 22 + 18);
-            middleAge = middleAge + firstTeamPlayers[i];         }
+            middleAge = middleAge + firstTeamPlayers[i];
+        }
 
-        System.out.println("Середній вік гравців першої команди");
+        System.out.println("Вік гравців першої команди");
         System.out.println(Arrays.toString(firstTeamPlayers));
         System.out.println();
         System.out.println("Середній вік гравців першої комманди: " + middleAge / firstTeamPlayers.length);
         System.out.println();
 //              Друга комманда
-        int middleAgeTwo  = 0;
+        int middleAgeTwo = 0;
         for (int i = 0; i < secondTeamPlayers.length; i++) {
             secondTeamPlayers[i] = (int) (Math.random() * 22 + 18);
-            middleAgeTwo = middleAgeTwo + secondTeamPlayers[i];         }
+            middleAgeTwo = middleAgeTwo + secondTeamPlayers[i];
+        }
 
         System.out.println("Вік гравців другої команди");
         System.out.println(Arrays.toString(secondTeamPlayers));
         System.out.println();
         System.out.println("Середній вік гравців другої комманди: " + middleAgeTwo / secondTeamPlayers.length);
-}
     }
+}
