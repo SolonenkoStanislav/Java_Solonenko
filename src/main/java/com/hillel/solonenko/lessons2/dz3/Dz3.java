80c2e25bd2ca6a56ce3b318f661bf7d3289ae69f
Solonenko Stanislav <solonenkostas@gmail.com>
2023-10-05 14:05:47 +0200

    done

---
+++ src/main/java/com/hillel/solonenko/lessons2/dz3/Dz3.java
@@ -0,0 +1,15 @@
+package com.hillel.solonenko.lessons2.dz3;
+
+public class Dz3 {
+    public static void main(String[] args) {
+          int a = 3;
+          int b = 4;
+          int c = 5;
+
+          int volume = 4*(a + b + c);
+          int length = a + b + c;
+
+        System.out.println("Довжина сторон = " + length);
+        System.out.println("Обьем = " + volume);
+    }
+}
